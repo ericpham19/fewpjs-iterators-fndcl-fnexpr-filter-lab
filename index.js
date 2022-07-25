7b650391f21c79dcf5743460d51b3baaea5d7e99
Duy Pham <ericpham19@outlook.com>
2022-07-25 11:34:56 +1000

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, attribute){
+        return drivers.filter(function (driver){return driver.toLowerCase() === attribute.toLowerCase()})
+}               
+    
+
+/* Write fuzzyMatch - This function takes an array of drivers and a string as arguments for querying the array,
+ and returns all drivers whose names begin with the provided letters.*/
+
+ function fuzzyMatch( drivers, attribute){
+    return drivers.filter (function (driver){ return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase()})
+ }
+
+ function matchName (drivers, value){ 
+    return drivers.filter (function (driver){return driver.name === value})
+ }
